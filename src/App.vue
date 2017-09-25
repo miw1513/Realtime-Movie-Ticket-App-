94e87560bf76f376b5050d46b16c83da6b7cdc48
miw1513 <miw1513@hotmail.com>
2017-09-25 18:56:19 +0700

    updated

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<<template>
+  <div class="box">
+    <h3 class="title">App {{ movieId }}</h3>
+    <Movie @chooseMovie="handleChooseMovie" :movieId="movieId" />
+    <seat @chooseSeat="handleChooseSeat" :movieId="movieId" />
+  </div>
+  
+  
+  
+  
+</template>
+
+<script>
+  import Movie from './Movie.vue'
+  import Seat from './Seat.vue'
+
+  export default {
+    components: { Movie , Seat },
+    data() {
+      return {
+        movieId: '',
+        selectSeats:[]
+      }
+    },
+    methods: {
+      handleChooseMovie(movieId){
+        console.log(movieId)
+        this.movieId = movieId
+      },
+      handleChooseSeat(seat){
+        const ids = this.selectSeats.map(s => s.id)
+        const idx = ids.indexOf(seat.id)
+        if (idx === -1){
+        this.selectSeats.push(seat)
+        }
+        
+        console.log(ids)
+      }
+    }
+  }
+</script>
---
+++ src/Seat.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="box">
+           <template v-for="s in seats">
+               <button 
+               :class="className(s)"
+               :disabled="s.seated"
+               @click="chooseSeat(s)"
+               >{{ s.id}} {{s.price}} </button>
+               <span>&nbsp;</span>
+               </template>
+            
+    </div>
+</template>
+
+<script>
+import movie from './movie.json'
+
+    export default {
+
+        props: [ 'movieId' ],
+        methods: {
+            className(seat){
+                return [
+                    'button' , { 'is-danger': seat.seated}
+                ]
+            },
+            chooseSeat(seat){
+                this.$emit('chooseSeat',seat)
+            }
+        },
+        computed: {
+            seats(){
+                
+               return movie[this.movieId]
+            }
+        }
+
+    }
+</script>
